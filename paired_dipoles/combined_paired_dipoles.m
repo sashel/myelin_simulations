a423844dbd5d8b15466292d1b2705f22ecb891c4
Saskia Helbling <saskia.helbling@gmail.com>
2024-11-20 18:52:37 +0100

    Adapt to new scaling

--- paired_dipoles/combined_paired_dipoles.m
+++ paired_dipoles/combined_paired_dipoles.m
@@ -1,4 +1,4 @@
-%% Set/prepare folder structure
+% prepare folder structure
 myelin_sim_dir = '<SIM_DIR>/myelin_sim_paired/';
 myelin_sim_dir_single_dipoles = '<SIM_DIR>/myelin_sim/'; % to get the vertices for scaling the leadfields
 
@@ -16,24 +16,24 @@ if ~ isfolder('paired_dipoles')
 end
 cd ..
 
-% we use the single dipole simulations as a starting point (the leadfield matrix, patch locations,..)
+% use single dipole simulations as a starting point (the leadfield matrix, patch locations,..)
 copyfile([myelin_sim_dir_single_dipoles '/Sim_MEG_data_coreg_err_0.*'],myelin_sim_dir,'f')
 copyfile([myelin_sim_dir_single_dipoles '/fixed_patches.mat'],myelin_sim_dir,'f')
 copyfile([myelin_sim_dir_single_dipoles '/patch_loc_ind_60_bilateral.mat'],myelin_sim_dir,'f')
 copyfile([myelin_sim_dir_single_dipoles '/U.mat'],myelin_sim_dir,'f')
 copyfile([myelin_sim_dir_single_dipoles '/SPMgainmatrix_Sim_MEG_data_coreg_err_0_1.mat'],[myelin_sim_dir 'SPMgainmatrix_Sim_MEG_data_coreg_err_0_1.mat'],'f')
 
-%% Set simulation parameters
+% set simulation parameters
 dipole_mom = [20 6]; % dipole strength in nAm and dipole patch size
 SNR = [0, -5, -10, -15, -20];% SNR in dB
 source_recon_all = {'IID','EBB','MSP'}; % source priors Bayesian source reconstruction
 rep_idx = 1:30; % simulated single dipole sources for each set
 
-scale = [1 3/2 2/3 4/3 3/4 11/10 10/11 21/20 20/21]; % scaling factors
+scale = [1 10 1/10 5 1/5 3 1/3 100 0.01]; % scaling factors
 save('scaling','scale')
-source_recon_idx = 1:3; % change if you want to perform simulations for specific source reconstruction appraoches only
+source_recon_idx = 1:3; % change if you want to perform simulations for specific source reconstruction approaches only
 
-%% Generate single dipole sources across 60 dipole locations
+% generate single dipole sources across 60 dipole locations
 orig_data = [myelin_sim_dir '/Sim_MEG_data_coreg_err_0.mat'];
 D = spm_eeg_load(orig_data);
 D.inv{1}.gainmat = 'SPMgainmatrix_Sim_MEG_data_coreg_err_0_1.mat';
@@ -48,7 +48,7 @@ for a = source_recon_idx
         elseif setNr == 2
             patch_loc_ind_sources = (1:30) + 30;
         end
-        for sim_i = 1:7 % sim_scale
+        for sim_i = 1:length(scale)
             for j = rep_idx
                 % simulate paired dipoles
                 vert_idx = Ip(patch_loc_ind_sources(j)); % current source location index
@@ -71,9 +71,9 @@ for a = source_recon_idx
 
                     load('SPMgainmatrix_Sim_MEG_data_coreg_err_0_1.mat','G','label')
                     load(sprintf('%srep%d_params_%d',myelin_sim_dir_single_dipoles,j,setNr),'weighted_x','x')
-                    G(:,x) = G(:,x)+G(:,x).*(scale(sim_i)-scale(1)).*repmat(full(weighted_x)',size(G,1),1)*length(x);
+                    G(:,x) = G(:,x)+G(:,x).*(scale(sim_i)-scale(1)).*repmat(full(weighted_x)',size(G,1),1));
                     load(sprintf('%srep%d_params_%d',myelin_sim_dir_single_dipoles,j,mod(setNr,2)+1),'weighted_x','x')
-                    G(:,x) = G(:,x)+G(:,x).*(1./scale(sim_i)-scale(1)).*repmat(full(weighted_x)',size(G,1),1)*length(x);
+                    G(:,x) = G(:,x)+G(:,x).*(1./scale(sim_i)-scale(1)).*repmat(full(weighted_x)',size(G,1),1);
                     save(sprintf('adapted_SPMgainmatrix_Sim_MEG_data_coreg_err_0_1.mat'),'G','label')
 
                     D = spm_eeg_load(orig_data);
@@ -93,17 +93,18 @@ for a = source_recon_idx
                     clear matlabbatch
                     close all
 
-                    for i = 1:length(scale) % do source reconstruction across scaling parameters
+                    % the perform source reconstruction across forward models with different scaling parameters
+                    for i = 1:length(scale) 
                         load('SPMgainmatrix_Sim_MEG_data_coreg_err_0_1.mat')
                         load(sprintf('%srep%d_params_%d',myelin_sim_dir_single_dipoles,j,setNr),'weighted_x','x')
-                        G(:,x) = G(:,x)+G(:,x).*(scale(i)-scale(1)).*repmat(full(weighted_x)',size(G,1),1)*length(x);
+                        G(:,x) = G(:,x)+G(:,x).*(scale(i)-scale(1)).*repmat(full(weighted_x)',size(G,1),1);
                         load(sprintf('%srep%d_params_%d',myelin_sim_dir_single_dipoles,j,mod(setNr,2)+1),'weighted_x','x')
-                        G(:,x) = G(:,x)+G(:,x).*(1./scale(i)-scale(1)).*repmat(full(weighted_x)',size(G,1),1)*length(x);
+                        G(:,x) = G(:,x)+G(:,x).*(1./scale(i)-scale(1)).*repmat(full(weighted_x)',size(G,1),1);
                         save(sprintf('adapted_SPMgainmatrix_Sim_MEG_data_coreg_err_0_1.mat'),'G','label')
 
-                        rep_data = [myelin_sim_dir '/rep_Sim_MEG_data_coreg_err_0.mat']; % output file with simulated single dipole data from output from Simulation_Batch_singleDipole_job.m
+                        rep_data = [myelin_sim_dir '/rep_Sim_MEG_data_coreg_err_0.mat']; 
                         D = spm_eeg_load(rep_data);
-                        D.inv{1}.gainmat = 'adapted_SPMgainmatrix_Sim_MEG_data_coreg_err_0_1.mat';
+                        D.inv{1}.gainmat = 'adapted_SPMgainmatrix_Sim_MEG_data_coreg_err_0_1.mat'; % used gainmatrix with adapted leadfields
                         D.save
 
                         matlabbatch{1}.spm.meeg.source.invertiter.D = {rep_data};
@@ -135,7 +136,7 @@ for a = source_recon_idx
                         % store results
                         R2(s,j,i) = D.inv{1}.inverse.R2;
                         VE(s,j,i) = D.inv{1}.inverse.VE; % explained variance
-                        F(s,j,i) = D.inv{1}.inverse.F; % negative Free Energy
+                        F(s,j,i) = D.inv{1}.inverse.F; % negative free energy
                     end
                 end
             end
